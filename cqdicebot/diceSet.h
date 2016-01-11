6dcf4741b2fa99cf5cc5f800c53554a50fff025f
im foolish <532468158@qq.com>
2016-01-12 03:59:25 +0800

    finding way to make command readable

--- cqdicebot/diceSet.h
+++ cqdicebot/diceSet.h
@@ -1,15 +1,17 @@
 #pragma once
 #include <random>
+#include <string>
 using namespace std;
 
 random_device rdd;
 mt19937 mtrand(rdd());
+
+#define MAX_DICE 100
+#define MAX_FACE 1000
+
 /*
 A single dice roll, for example +4d6, -8d6k3.
 */
-
-const int MAX_DICE = 100;
-
 class diceSet
 {
 private:
@@ -18,18 +20,35 @@ private:
 	int numOfFaces;
 	int numOfKeep;
 public:
-	diceSet(int sig, int dice, int face, int keep = -1) {
-		sign = sig;
+	const static int GETDICE_DONE = 0;
+	const static int GETDICE_FAIL = 1;
+
+	diceSet() {
+		sign = 1;
+		numOfDice = 0;
+		numOfFaces = 1;
+		numOfKeep = -1;
+	}
+	diceSet(int sig, int dice, int face = 1, int keep = -1) {
+		sign = sig < 0 ? -1 : 1;
+		numOfDice = dice > MAX_FACE ? MAX_FACE : dice;
+		numOfFaces = face > MAX_FACE ? MAX_FACE : face;
+		numOfKeep = keep;
+	}
+	void reset(int sig, int dice, int face = 1, int keep = -1) {
+		sign = sig < 0 ? -1 : 1;
 		numOfDice = dice;
 		numOfFaces = face;
 		numOfKeep = keep;
 	}
 	int throwDice() {
-		if (numOfDice > MAX_DICE) return -1;
-		if (numOfKeep == 0)return 0;
+		if (numOfDice > MAX_DICE) return -1;//too many dice
+		if (numOfFaces > MAX_FACE) return -1;//too many faces
+		if (numOfDice <= 0) return 0;//no dice rolled
+		if (numOfFaces <= 1) return numOfDice;//just a value, not dice
+		if (numOfKeep <= 0)return 0;//nothing left if you keep nothing
 
 		int ret = 0;
-
 		if (numOfKeep < 0 || numOfKeep>numOfDice) {
 			for (int i = 0; i < numOfDice; i++) {
 				ret += (mtrand() % numOfFaces + 1);
@@ -44,15 +63,66 @@ public:
 					for (int j = 0; j < numOfKeep; i++)
 						if (out[j] < rand) { out[j] = rand; break; }
 				}
-				else {
-					out[i] = (mtrand() % numOfFaces + 1);
-				}
+				else { out[i] = (mtrand() % numOfFaces + 1); }
 			}
-
-			for (int i = 0; i < numOfKeep; i++) {
+			for (int i = 0; i < numOfKeep; i++)
 				ret += out[i];
-			}
 		}
 		return (ret * sign);
+	}
+
+	int getDice(string str) {
+		size_t split = str.find_first_of("+-",1);
+		int endOfRoll;
+		if (split == string::npos) endOfRoll = str.length() - 1;
+		else endOfRoll = split - 1;
+
+		bool flag_no_dice;
+		bool flag_no_keep;
+		bool flag_has_sign;
+		int sign = 1, dice, face, keep = -1;
+		//1d100+2d200
+		//split = 5
+		//
+		flag_has_sign = (str[0] == '+' || str[0] == '-');
+		string roll_string = str.substr(flag_has_sign ? 1 : 0, split);
+		if (flag_has_sign) sign = (str[0] == '+' ? 1 : -1);
+
+		size_t seq_d = roll_string.find_first_of("Dd");
+		if (seq_d == string::npos) {
+			//+6
+			flag_no_dice = true;
+			dice = atoi(roll_string.c_str());
+			if (dice <= 0 || dice == INT32_MAX || dice == INT32_MIN) return GETDICE_FAIL;
+			reset(sign, dice, 1, keep);
+			return GETDICE_DONE;
+		}
+
+		size_t seq_k = roll_string.find_first_of("Kk");
+		flag_no_keep = (seq_k == string::npos);
+		
+		string dice_string = roll_string.substr(0, seq_d);
+		dice = atoi(dice_string.c_str());
+		if (dice <= 0 || dice == INT32_MAX || dice == INT32_MIN) return GETDICE_FAIL;
+
+		if (flag_no_keep) {
+			string face_string = roll_string.substr(seq_d + 1);
+			face = atoi(face_string.c_str());
+			if (face <= 0 || face == INT32_MAX || face == INT32_MIN) return GETDICE_FAIL;
+		}
+		else {
+			//3d100k2
+			//seq_d = 1
+			//seq_k = 5
+			//length = 7
+			string face_string = roll_string.substr(seq_d, roll_string.length() - seq_k - 1);
+			face = atoi(face_string.c_str());
+			if (face <= 0 || face == INT32_MAX || face == INT32_MIN) return GETDICE_FAIL;
+			string keep_string = roll_string.substr(seq_k + 1);
+		}
+
+		reset(sign, dice, face, keep);
+
+		return GETDICE_DONE;
 	}
 };
--- cqdicebot/diceStringParser.h
+++ cqdicebot/diceStringParser.h
@@ -1,5 +1,17 @@
 #include <string>
+#include <list>
+#include "diceSet.h"
 using namespace std;
+
+const int PARSE_STANDBY = 1;//0001 No string is parsed right now.
+const int PARSE_FINISHED_SUCCESSFULLY = 0;//0000 A command string is parsed successfully.
+const int PARSE_FINISHED_ERROR = 4;//0100 Something bad happens in parsing.
+const int PARSE_ERROR_ILL_INPUT = 4;//0100 The input is not written properly.
+const int PARSE_ERROR_DICE_EXCEED_MAXIMUM = 6;//0110 Too much dice or a die has too many faces.
+
+const int PARSE_MODE_ROLL = 0;//00 command for rolling some dice
+const int PARSE_MODE_HIDE = 1;//01 command for rolling some dice behind screen
+const int PARSE_MODE_NAME = 2;//10 command for changing name
 
 /*
 String parser specified for .r command and so on.
@@ -9,12 +21,16 @@ class diceStringParser
 private:
 	string ret;
 	int status = PARSE_STANDBY;
+	int mode = PARSE_MODE_ROLL;
+	string roll_name;
+	string roll_list;
+	list<diceSet> diceseries;
+
+	int getDice(diceSet &ds) {
+
+	}
+
 public:
-	const int PARSE_STANDBY = 1;//0001 No string is parsed right now.
-	const int PARSE_FINISHED_SUCCESSFULLY = 0;//0000 A command string is parsed successfully.
-	const int PARSE_FINISHED_ERROR = 4;//0100 Something bad happens in parsing.
-	const int PARSE_ERROR_ILL_INPUT = 4;//0100 The input is not written properly.
-	const int PARSE_ERROR_DICE_EXCEED_MAXIMUM = 6;//0110 Too much dice or a die has too many faces.
 	diceStringParser() {
 
 	};
@@ -24,38 +40,62 @@ public:
 	/*
 	Input target string. This method will update the inner status of object.
 	*/
-	void Parse(const char * msg) {
-		if (msg[0] != '.') { status = PARSE_ERROR_ILL_INPUT; return; }
-		string input = msg;
-
+	int Parse(const char * msg) {
+		if (msg[0] != '.') { status = PARSE_ERROR_ILL_INPUT; return status; }
+		string input_ori = msg;
+		int input_ori_length = input_ori.length();
+		string input_trim = msg;
 		//fuck this std::string without trim()
-		int i = input.length() - 1;
+		int i = input_ori_length - 1;
 		while (i > 0) {
-			if (input[i] == ' '
-				|| input[i] == '\n'
-				|| input[i] == '\r'
+			if (input_trim[i] == ' '
+				|| input_trim[i] == '\n'
+				|| input_trim[i] == '\r'
 				) {
-				input.erase(i);
+				input_trim.erase(i);
 			}
 			i--;
 		}
+		int input_trim_length = input_trim.length();
 
-		if (input.length() < 3) { status = PARSE_ERROR_ILL_INPUT; return; }
-		
-		int command_end = input.find_first_of("1234567890+-") - 1;
-		string command = input.substr(0, command_end);
+		if (input_trim.length() < 3) { status = PARSE_ERROR_ILL_INPUT; return status; }
+
+		//.r1d10abcde
+		//command_end = 1
+		//roll_name_loc = 5
+		//input_ori_length = 11
+		//
+		size_t command_end = input_trim.find_first_of("1234567890+-") - 1;
+		if(command_end == string::npos) { status = PARSE_ERROR_ILL_INPUT; return status; }
+		string command = input_trim.substr(0, command_end + 1);
+
 		if (command.compare(".r") == 0
 			|| command.compare(".R") == 0) {
+			mode = PARSE_MODE_ROLL;
+			int roll_name_loc = input_ori.find_last_of("1234567890") + 1;
+			if (roll_name_loc < input_ori_length) { roll_name = input_trim.substr(roll_name_loc, input_ori_length - roll_name_loc - 1); }
+			else roll_name = "";
+			roll_list = input_trim.substr(command_end + 1, roll_name_loc - command_end);
+
+			diceSet ds;
+			while (roll_list.length() > 0)
+			{
+				int lap = ds.getDice(roll_list);
+				if (lap == diceSet::GETDICE_FAIL) continue;
+				else diceseries.push_back(ds);
+			};
 		}
 		else if (command.compare(".h") == 0
 			|| command.compare(".H") == 0) {
+			mode = PARSE_MODE_HIDE;
 
-		}
-		else if (command.compare(".d") == 0
-			|| command.compare(".D") == 0) {
 		}
 		else if (command.compare(".n") == 0
 			|| command.compare(".N") == 0) {
+			mode = PARSE_MODE_ROLL;
+		}
+		else {
+			status = PARSE_ERROR_ILL_INPUT; return status;
 		}
 	};
 	/*
